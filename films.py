f03a320fbff5d9e41a451a49136dbfd7d76f5aab
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2016-04-10 22:37:14 +0200

    Add automatic redirect after search

--- films.py
+++ films.py
@@ -1,9 +1,6 @@
-import requests
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
 from os.path import splitext
+
+import requests
 
 import data
 import config
@@ -14,35 +11,37 @@ from db import session_scope
 def add_film_with_session(pathname, session):
     title = splitext(pathname)[0]
     d = data.search(title)
-    
+
     if isinstance(d, data.MovieData):
         entry = Movie(pathname=pathname, **d.basic_info())
     else:
         entry = TV(pathname=pathname, **d.basic_info())
-    
+
     for genre_name in d.genres():
         genre = session.query(Genre).filter_by(name=genre_name).first()
         if not genre:
             genre = Genre(name=genre_name)
         entry.genres.append(genre)
-    
+
+    # TODO: Remove duplicates from d.cast()
+    # Test with add_film("Seven Samurai")
     for order, actor_name, role in d.cast():
         with session.no_autoflush:
             actor = session.query(Actor).filter_by(name=actor_name).first()
             if not actor:
                 actor = Actor(name=actor_name)
-            
+
             performance = Performance(role=role)
             performance.actor = actor
             entry.performances.append(performance)
         session.flush()
-    
+
     session.add(entry)
-    
+
     url = d.poster_url()
     r = requests.get(url)
     r.raise_for_status()
-    
+
     session.commit()
     with open(config.IMAGE_DIR + '/%s.jpg' % entry.id, 'wb') as f:
         f.write(r.content)
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import random
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
+from flask import send_from_directory
 
 from db import Session
 from models import Media
@@ -12,34 +15,40 @@ app = Flask(__name__)
 def main():
     session = Session()
     films = session.query(Media).all()
-    
+
     for film in films:
         film.tmdb_rating = round(film.tmdb_rating, 1)
-    
+
     random.shuffle(films)
     favorites = films[:5]
     random.shuffle(films)
     recently_viewed = films[:5]
     random.shuffle(films)
     recently_added = films[-5:]
-    
+
     return render_template('index.html', favorites=favorites, recently_viewed=recently_viewed, recently_added=recently_added)
 
 
 @app.route('/film/<id>')
-def film(id):
+def film(id): # pylint: disable=invalid-name
     session = Session()
     film = session.query(Media).filter_by(id=id).one()
     film.tmdb_rating = round(film.tmdb_rating, 1)
-    
+
     return render_template('film.html', film=film)
 
 
 @app.route('/search')
 def search():
+    title = request.args.get('title', '')
     session = Session()
-    films = session.query(Media).all()[:5]
-    
+    films = session.query(Media).filter(Media.title.ilike("%{}%".format(title))).all()
+
+    # Presumptuous to redirect?
+    # https://ux.stackexchange.com/questions/76575/should-i-redirect-the-user-if-theres-only-one-search-result-or-still-show-the
+    if len(films) == 1 and films[0].title.lower() == title.lower():
+        return redirect(url_for('film', id=films[0].id))
+
     return render_template('search.html', films=films)
 
 
@@ -51,3 +60,4 @@ def send_static(path):
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
+#    app.run(debug=False, host='0.0.0.0')
